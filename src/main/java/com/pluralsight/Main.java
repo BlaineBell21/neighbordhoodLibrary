cfc2cf1f6633330f3843d133fab643aa8c354f2f
Blaine Bell <blainebell21@gmail.com>
2026-04-20 15:35:08 -0700

    asked instructor remsey for help and fixed issue with program not terminating after entering in exit issue was that after exiting the book check out method, it would return to the main menu without outputting the menu screen options

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,8 +1,5 @@
 package com.pluralsight;
 
-import static com.pluralsight.InputScanner.userInputInt;
-import static com.pluralsight.InputScanner.userInputString;
-
 
 public class Main {
     public static void main(String[] args){
